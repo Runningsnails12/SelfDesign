fc6dba0127241fdbfd423863eefd1db46b74eeaa
running_snails <1321973708@qq.com>
2022-01-22 22:07:35 +0800

    init

---
+++ src/api/index.js
@@ -0,0 +1,15 @@
+/**
+ * 例如
+ * login(params) {
+        return request({
+            url: '/users/login',
+            method: 'POST',
+            data: params,         //传输的数据
+            mock: true            //是否开启mock,默认(没有写)是不开启
+        })
+    },
+ */
+import request from '../utils/request'
+export default {
+
+}
---
+++ src/config/index.js
@@ -0,0 +1,21 @@
+/**
+ * 环境配置
+ * dev是开发环境,prod是生产环境,如果开启mock他的根路径是mockApi,否则是baseApi
+ */
+const env = import.meta.env.MODE || 'prod';
+const EnvConfig = {
+    dev: {
+        baseApi: '/api',
+        mockApi: 'https://www.fastmock.site/mock/c213efb03a5cad1915f73f29ba1080b0/api'
+    },
+    prod: {
+        baseApi: '/api',
+        mockApi: 'https://www.fastmock.site/mock/c213efb03a5cad1915f73f29ba1080b0/api'
+    }
+}
+export default {
+    env,
+    mock: false,//是否全局开启mock
+    namespace: 'selfDesign',
+    ...EnvConfig[env]
+}
