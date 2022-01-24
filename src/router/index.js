96c601c968ff3716d5eca1d9c04dcb7122572ed7
tomtiao <tomtiao1234@gmail.com>
2022-01-24 22:06:42 +0800

    chore: use history router

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = []
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
