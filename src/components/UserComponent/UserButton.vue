89219727102cc901045ada01eb7cd44b5f0e800e
tomtiao <tomtiao1234@gmail.com>
2022-01-25 17:42:24 +0800

    chore: add userland component

---
+++ src/components/UserComponent/UserButton.vue
@@ -0,0 +1,31 @@
+<script setup>
+</script>
+
+<template>
+  <button type="button">
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+button {
+  color: #666666;
+  font-size: 16px;
+
+  background-color: #F5F5F5;
+  border-radius: 6px 6px 6px 6px;
+  border: 1px solid #DCDFE6;
+  padding: 0.75em 1em;
+
+  cursor: pointer;
+
+  transition: background-color 66ms ease-in;
+}
+
+button:hover {
+  color: white;
+
+  background-color: #999999;
+  border: 1px solid #666666;
+}
+</style>
---
+++ src/components/UserComponent/UserInput.vue
@@ -0,0 +1,59 @@
+<script setup>
+const props = defineProps({
+  value: {
+    type: String,
+    default: ''
+  },
+  type: {
+    type: String,
+    required: true
+  },
+  rows: {
+    type: [Number, String],
+    default: 3
+  },
+  dashed: Boolean
+});
+
+const emit = defineEmits({
+  'update:value': (payload) => typeof payload === 'string'
+});
+
+const onInput = (e) => {
+  emit('update:value', e.target.value);
+};
+</script>
+
+<template>
+  <input
+    v-if="type == 'text'"
+    type="text"
+    class="dashed"
+
+    :value="props.value"
+    @input="onInput"
+  >
+  <textarea
+    v-if="type == 'textarea'"
+    cols="30"
+    :rows="props.rows"
+    :class="{ dashed: props.dashed }"
+
+    :value="props.value"
+    @input="onInput"
+  />
+</template>
+
+<style scoped>
+.dashed {
+  border-style: dashed;
+}
+
+input {
+  outline: none;
+}
+
+textarea {
+  outline: none;
+}
+</style>
