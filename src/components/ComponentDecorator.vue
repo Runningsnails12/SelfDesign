9eb640f18e03bc4b8d95416939c78e7e803ddba5
tomtiao <tomtiao1234@gmail.com>
2022-01-27 19:10:03 +0800

    feat: add ComponentDecorator as an abstract layer

---
+++ src/components/ComponentDecorator.vue
@@ -0,0 +1,46 @@
+<script>
+import { computed, defineComponent, h } from 'vue';
+import { useStore } from 'vuex';
+
+export default defineComponent({
+  props: {
+    id: {
+      type: [String, null],
+      required: true
+    },
+    transformer: {
+      type: Function,
+      default: (rawData) => rawData
+    }
+  },
+  emits: ['click'],
+  setup(props, { emit, slots }) {
+    if (!slots.default) return () => '空的';
+
+    const store = useStore();
+
+    const rawData = computed(() => store.state.components.get(props.id) ?? null);
+
+    const data = computed(() => props.transformer(rawData.value));
+
+    const emitClickWrapper = (cb) =>
+      (e) => {
+        emit('click', e);
+        if (cb) cb(e);
+      };
+
+    return () =>
+      slots.default({ data: data.value })
+        .map(
+          comp => h(
+            comp.type,
+            {
+              ...comp.props,
+              onClick: emitClickWrapper(comp.props.onClick)
+            },
+            comp.children
+          )
+        );
+  }
+});
+</script>
