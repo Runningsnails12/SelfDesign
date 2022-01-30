d8c45a7cb6f3c7f664c0cb01fbf00afea5aea515
running_snails <1321973708@qq.com>
2022-01-30 12:18:49 +0800

    feat: data format

--- src/App.vue
+++ src/App.vue
@@ -9,16 +9,37 @@ import api from './api';
 
 const store = useStore();
 onMounted(async () => {
-  store.commit('setToken',localStorage.getItem('token'));
+  store.commit('setToken', localStorage.getItem('token'));
   const { data } = await api.getUserInfo();
-  if (data){
+  if (data) {
     store.commit('setUsername', data.username);
-  }else{
+  } else {
     Message.error('登录过期了，请重新登录');
-    store.commit('setToken',undefined);
+    store.commit('setToken', undefined);
     store.commit('handleLoginFormClose');
   }
 });
+
+Date.prototype.format = function (fmt) {
+  var o = {
+    'M+': this.getMonth() + 1,                 //月份 
+    'd+': this.getDate(),                    //日 
+    'h+': this.getHours(),                   //小时 
+    'm+': this.getMinutes(),                 //分 
+    's+': this.getSeconds(),                 //秒 
+    'q+': Math.floor((this.getMonth() + 3) / 3), //季度 
+    'S': this.getMilliseconds()             //毫秒 
+  };
+  if (/(y+)/.test(fmt)) {
+    fmt = fmt.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
+  }
+  for (var k in o) {
+    if (new RegExp('(' + k + ')').test(fmt)) {
+      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)));
+    }
+  }
+  return fmt;
+};
 
 </script>
 
