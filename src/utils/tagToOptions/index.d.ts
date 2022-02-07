1a72b903c1b09b0e81b18522807ead93762d61b9
tomtiao <tomtiao1234@gmail.com>
2022-02-08 00:13:43 +0800

    chore: update comments

--- src/utils/tagToOptions/index.d.ts
+++ src/utils/tagToOptions/index.d.ts
@@ -13,8 +13,8 @@ export type Result = {
 };
 /**
  * 根据标签名返回组件选项
- * 用法见 src/utils/tagToOptions/README.md
+ * @see 用法见 src/utils/tagToOptions/README.md
  * @param tag 标签名
- * @return 一个标签对应的选项的对象：{ options: 组件自身的选项及其类型; subOptions（可能有）: 布局组件的子元素选项及其类型; values（可能有）: 组件特殊值及其类型; event（可能有）: 组件事件类型 }；如果标签名不存在，则返回 null
+ * @return 一个标签对应的选项的对象；如果标签名不存在，则返回 null
  */
 export function tagToOptions(tag: string): Result | null;
